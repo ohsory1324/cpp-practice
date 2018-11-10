3a79dfe4a6404936cc4e963607d0e673302184a0
Yungoo Kong <ohsory1324@gmail.com>
2018-11-10 14:11:04 +0900

    init

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "\'Hello World!\'" << std::endl;
+  std::cout << "\"Hello World!\"" << std::endl;
+  std::cout << "!@#$%^&*()" << std::endl;
+  printf("hello\n");
+  printf("hello\n");
+  printf( "%-20s %-20s %5s\n" , "Name" , "Surname" , "Id" );
+  return 0;
+}
---
+++ input.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main()
+{
+  // int a;
+  // printf("정수를 입력해주세요: ");
+  // scanf("%d", &a);
+  // printf("%d\n", a);
+
+  // char b;
+  // printf("문자를 입력해주세요: ");
+  // scanf("%c", &b);
+  // printf("%c\n", b);
+
+  // float c;
+  // printf("실수를 입력해주세요: ");
+  // scanf("%f", &c);
+  // printf("%f\n", c);
+
+  // int n1, n2;
+  // printf("정수 두개를 입력해주세요(ex: 2 0): ");
+  // scanf("%d %d", &n1, &n2);
+  // printf("%d %d\n", n1, n2);
+
+  // char c1, c2;
+  // printf("문자 두개를 입력해주세요(ex: a B): ");
+  // scanf("%c %c", &c1, &c2);
+  // printf("%c %c\n", c2, c1);
+
+  // float f;
+  // printf("실수를 입력해주세요: ");
+  // scanf("%f", &f);
+  // printf("%.2f\n", f);
+
+  double dbl;
+  printf("실수를 입력해주세요: ");
+  scanf("%lf", &dbl);
+  printf("%.2lf\n", dbl);
+
+  return 0;
+}
